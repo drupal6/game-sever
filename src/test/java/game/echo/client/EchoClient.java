bb3cf379d44cc41a314e246ee0b5058c15d54358
Administrator <Administrator@WIN-H30Q2CPAMEI>
2014-04-24 21:30:57 +0800

    echo damo

---
+++ src/test/java/game/echo/client/EchoClient.java
@@ -0,0 +1,42 @@
+package game.echo.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class EchoClient {
+	
+	public static void main(String[] arg) {
+		EchoClient c = new EchoClient();
+		c.run("127.0.0.1", 8080, 100);
+	}
+	
+	public void run(String host, int port, final int size) {
+		EventLoopGroup loopGroup = new NioEventLoopGroup();
+		try {
+			Bootstrap b = new Bootstrap();
+			b.group(loopGroup)
+			.channel(NioSocketChannel.class)
+			.option(ChannelOption.TCP_NODELAY, true)
+			.handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel paramC) throws Exception {
+					paramC.pipeline().addLast(new EchoClientHandler(size));
+				}
+			});
+			ChannelFuture f = b.connect(host, port).sync();
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			loopGroup.shutdownGracefully();
+		}
+		
+	}
+
+}
---
+++ src/test/java/game/echo/client/EchoClientHandler.java
@@ -0,0 +1,77 @@
+package game.echo.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoClientHandler extends ChannelInboundHandlerAdapter {
+	
+	 private final ByteBuf firstMessage;
+	 
+	 public EchoClientHandler(int firstMessageSize) {
+		 if(firstMessageSize <= 0) {
+			 throw new IllegalArgumentException("firstMessageSize:" + firstMessageSize);
+		 }
+		 firstMessage = Unpooled.buffer(firstMessageSize);
+			for (int i = 0; i < firstMessage.capacity(); i ++) {
+			firstMessage.writeByte((byte) i);
+		 }
+	 }
+	
+	@Override
+	public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+		super.channelRegistered(ctx);
+		System.out.println("EchoClientHandler  channelRegistered");
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		super.channelActive(ctx);
+		System.out.println("EchoClientHandler  channelActive");
+		ctx.writeAndFlush(firstMessage);
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		super.channelInactive(ctx);
+		System.out.println("EchoClientHandler  channelInactive");
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		super.channelRead(ctx, msg);
+		System.out.println("EchoClientHandler  channelRead");
+		ctx.write(msg);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		super.channelReadComplete(ctx);
+		System.out.println("EchoClientHandler  channelReadComplete");
+		ctx.flush();
+	}
+
+	@Override
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt)
+			throws Exception {
+		super.userEventTriggered(ctx, evt);
+		System.out.println("EchoClientHandler  userEventTriggered");
+	}
+
+	@Override
+	public void channelWritabilityChanged(ChannelHandlerContext ctx)
+			throws Exception {
+		super.channelWritabilityChanged(ctx);
+		System.out.println("EchoClientHandler  channelWritabilityChanged");
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		super.exceptionCaught(ctx, cause);
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
